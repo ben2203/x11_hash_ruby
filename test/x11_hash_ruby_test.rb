102af7c144923f2151d579df0c801509500e98d2
Luke <lukem95@gmail.com>
2014-11-19 10:42:36 +0000

    Added correct X11 hash for hello

--- test/x11_hash_ruby_test.rb
+++ test/x11_hash_ruby_test.rb
@@ -4,7 +4,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/x11_hash_ruby.rb')
 class X11HashTest < Test::Unit::TestCase
 	def test_hash_hello
 		str = "hello"
-		expect = "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
+		expect = "7d 6d af 9f dd 03 4b bd fd 97 e0 f5 fb 97 5c 9a a0 92 a9 09 d0 f3 13 82 86 ff f4 6c e5 c4 c6 36"
 		got = X11Hash::Engine::hash str
 		assert_equal expect, got
 	end
